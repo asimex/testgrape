6bbccb792b11d3a617ce06e978c114b3a36c20b4
hereimsarif <sarif@sidatasolutions.com>
2025-05-13 19:09:21 +0530

    jscss

---
+++ Abc.js
@@ -0,0 +1,12 @@
+const Header = () => (
+    <header className="site-header">
+      <div className="logo">MySite</div>
+      <nav className="nav-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+  );
+  
