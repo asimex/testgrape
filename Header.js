cf7f5925abcbd48a3b080535e6e862b7a5d46ea6
hereimsarif <sarif@sidatasolutions.com>
2025-05-13 18:54:21 +0530

    changes

--- Header.js
+++ Header.js
@@ -1,4 +1,4 @@
-const Button = ({ title, content }) => (
+const Header = ({ title, content }) => (
     <div style={{ border: '1px solid #ccc', padding: '10px' }}>
       <h3>header</h3>
       <p>world</p>
