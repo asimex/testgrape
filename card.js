0ae0baf10a20af7561b0cb03b8ba9a6664e20cf4
hereimsarif <sarif@sidatasolutions.com>
2025-05-13 18:46:33 +0530

    card changed

--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-const SampleCard = ({ title, content }) => (
+const Card = ({ title, content }) => (
     <div style={{ border: '1px solid #ccc', padding: '10px' }}>
       <h3>{title}</h3>
       <p>{content}</p>
